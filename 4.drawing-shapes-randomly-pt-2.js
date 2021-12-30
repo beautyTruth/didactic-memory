7047bbc3d04bbc3a4a288d67151855d57e9f7949
Phil <happy.melodist@gmail.com>
2021-12-30 11:03:46 -0600

    review randomization of circles, arcs and lines in the 2d canvas context

--- 4.drawing-shapes-randomly-pt-2.js
+++ 4.drawing-shapes-randomly-pt-2.js
@@ -1,8 +1,8 @@
-const canvasEl = document.querySelector("canvas");
-const canvasContext = canvasEl.getContext("2d");
+// const canvasEl = document.querySelector("canvas");
+// const canvasContext = canvasEl.getContext("2d");
 
-canvasEl.height = window.innerHeight;
-canvasEl.width = window.innerWidth;
+// canvasEl.height = window.innerHeight;
+// canvasEl.width = window.innerWidth;
 
 // ---------------------------------------------Drawing Lines Randomly
 // Example 4 -> randomizing the lines location
@@ -43,16 +43,77 @@ canvasEl.width = window.innerWidth;
 // ---------------------------------------------Drawing Circles Randomly
 // Example 6 -> randomizing the circles location + color
 
+// for (let i = 0; i < 20; i++) {
+//   let XStart = Math.random() * window.innerWidth;
+//   let YStart = Math.random() * window.innerHeight;
+
+//   let radius = (Math.random() * window.innerWidth) / 10;
+
+//   canvasContext.beginPath();
+//   canvasContext.arc(XStart, YStart, radius, 0, Math.PI * 2);
+//   canvasContext.strokeStyle = "green";
+//   canvasContext.stroke();
+// }
+
+// ------------------------------------------------------------------------
+
+// today is a good day to have a good day
+
+// growth is painful. Change is Painful. But nothing is a painful as staying stuck somewhere where you don't belong
+
+// const canvasEl = document.querySelector("canvas");
+// const canvasContext = canvasEl.getContext("2d");
+
+// canvasEl.height = window.innerHeight;
+// canvasEl.width = window.innerWidth;
+
+const canvasEl = document.querySelector("canvas");
+const canvasCTX = canvasEl.getContext("2d");
+
+canvasEl.height = window.innerHeight;
+canvasEl.width = window.innerWidth;
+
+// -=-=-=-=-=-=--=-=-=-=-=-=-=-=- drawing lines randomly -=-=-=-=-=-=-=-=-=-=-=-=-
 for (let i = 0; i < 20; i++) {
-  let XStart = Math.random() * window.innerWidth;
-  let YStart = Math.random() * window.innerHeight;
+  let xStart = Math.random() * window.innerWidth;
+  let yStart = Math.random() * window.innerHeight;
+
+  let xEnd = Math.random() * window.innerWidth;
+  let yEnd = Math.random() * window.innerHeight;
+
+  canvasCTX.beginPath();
+  canvasCTX.moveTo(xStart, yStart);
+  canvasCTX.lineTo(xEnd, yEnd);
+  canvasCTX.strokeStyle = "fuchsia";
+  canvasCTX.lineWidth = 10;
+  canvasCTX.stroke();
+}
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= drawing arcs randomly -=-=-=-=-=-=-=-=-=-=-=-=-=
+
+for (let j = 0; j < 20; j++) {
+  let xStart = Math.random() * window.innerWidth;
+  let yStart = Math.random() * window.innerHeight;
 
   let radius = (Math.random() * window.innerWidth) / 10;
 
-  canvasContext.beginPath();
-  canvasContext.arc(XStart, YStart, radius, 0, Math.PI * 2);
-  canvasContext.strokeStyle = "green";
-  canvasContext.stroke();
+  canvasCTX.beginPath();
+  canvasCTX.arc(xStart, yStart, radius, 0, Math.PI);
+  canvasCTX.strokeStyle = "peachpuff";
+  canvasCTX.lineWidth = 25;
+  canvasCTX.stroke();
 }
 
-// ------------------------------------------------------------------------
+// =-=-=-=-=-=-=-=-=-=-=-=-=--=-=- randomizing circles location and color -=-=-=-=-=-
+
+for (let j = 0; j < 20; j++) {
+  let xStart = Math.random() * window.innerWidth;
+  let yStart = Math.random() * window.innerHeight;
+
+  let radius = (Math.random() * window.innerWidth) / 10;
+
+  canvasCTX.beginPath();
+  canvasCTX.arc(xStart, yStart, radius, 0, Math.PI * 2);
+  canvasCTX.strokeStyle = "lime";
+  canvasCTX.lineWidth = 25;
+  canvasCTX.stroke();
+}
