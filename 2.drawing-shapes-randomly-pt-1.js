1f4e2a16a1493262940f81cee578e99211e0b994
Phil <happy.melodist@gmail.com>
2021-12-30 09:20:38 -0600

    review randomization of rectangles placements, colors, and sizes

--- 2.drawing-shapes-randomly-pt-1.js
+++ 2.drawing-shapes-randomly-pt-1.js
@@ -1,8 +1,8 @@
-const canvasEl = document.querySelector("canvas");
-const canvasContext = canvasEl.getContext("2d");
+// const canvasEl = document.querySelector("canvas");
+// const canvasContext = canvasEl.getContext("2d");
 
-canvasEl.height = window.innerHeight;
-canvasEl.width = window.innerWidth;
+// canvasEl.height = window.innerHeight;
+// canvasEl.width = window.innerWidth;
 
 // ----------------------------------------------------------------Drawing Rectangles/Squares Randomly
 
@@ -12,7 +12,7 @@ canvasEl.width = window.innerWidth;
 //   let x = Math.random() * window.innerWidth;
 //   let y = Math.random() * window.innerHeight;
 
-//   canvasContext.fillStyle = "white";
+//   canvasContext.fillStyle = "fuchsia";
 //   canvasContext.fillRect(x, y, 50, 50);
 // }
 // -------------------------------------------------------------
@@ -33,7 +33,69 @@ canvasEl.width = window.innerWidth;
 
 // Example 3 -> randomizing the location + color + size
 
-for (let i = 1; i < 21; i++) {
+// for (let i = 1; i < 21; i++) {
+//   let x = Math.random() * window.innerWidth;
+//   let y = Math.random() * window.innerHeight;
+
+//   let red = Math.ceil(Math.random() * 255);
+//   let green = Math.ceil(Math.random() * 255);
+//   let blue = Math.ceil(Math.random() * 255);
+
+//   let width = Math.random() * 200;
+//   let height = Math.random() * 200;
+
+//   canvasContext.fillStyle = `rgb(${red}, ${green}, ${blue})`;
+//   canvasContext.fillRect(x, y, width, height);
+// }
+// -------------------------------------------------------------
+
+// it's a good day to have a good day
+
+/*
+my code below
+*/
+
+// const canvasEl = document.querySelector("canvas");
+// const canvasContext = canvasEl.getContext("2d");
+
+// canvasEl.height = window.innerHeight;
+// canvasEl.width = window.innerWidth;
+
+const canvasEl = document.querySelector("canvas");
+const canvasCTX = canvasEl.getContext("2d");
+
+canvasEl.height = window.innerHeight;
+canvasEl.width = window.innerWidth;
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= drawing rectangles and squares randomly =-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+// here, we randomize the location
+
+// for (let i = 1; i < 11; i++) {
+//   let x = Math.random() * window.innerWidth;
+//   let y = Math.random() * window.innerHeight;
+
+//   canvasCTX.fillStyle = "cyan";
+//   canvasCTX.fillRect(x, y, 50, 50);
+// }
+
+// =-=-=-=-=-=-=-=-=-=-=-=- randomization of the color and the location =-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// for (let i = 1; i < 30; i++) {
+//   let x = Math.random() * window.innerWidth;
+//   let y = Math.random() * window.innerHeight;
+
+//   let red = Math.ceil(Math.random() * 255);
+//   let green = Math.ceil(Math.random() * 255);
+//   let blue = Math.ceil(Math.random() * 255);
+
+//   canvasCTX.fillStyle = `rgb(${red}, ${green}, ${blue})`;
+//   canvasCTX.fillRect(x, y, 50, 50);
+// }
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= randomization of the color, location, and size =-=-=-=-=-=-=
+
+for (let i = 1; i < 30; i++) {
   let x = Math.random() * window.innerWidth;
   let y = Math.random() * window.innerHeight;
 
@@ -44,7 +106,6 @@ for (let i = 1; i < 21; i++) {
   let width = Math.random() * 200;
   let height = Math.random() * 200;
 
-  canvasContext.fillStyle = `rgb(${red}, ${green}, ${blue})`;
-  canvasContext.fillRect(x, y, width, height);
+  canvasCTX.fillStyle = `rgb(${red}, ${green}, ${blue})`;
+  canvasCTX.fillRect(x, y, width, height);
 }
-// -------------------------------------------------------------
