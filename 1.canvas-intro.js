d2e3fb3a51fcf29fbeec6ddad7bced696c29f299
Phil <happy.melodist@gmail.com>
2021-12-30 08:18:26 -0600

    complete introductory review to 2d canvas

--- 1.canvas-intro.js
+++ 1.canvas-intro.js
@@ -1,20 +1,44 @@
+// const canvasEl = document.querySelector("canvas");
+// const canvasContext = canvasEl.getContext("2d");
+
+// canvasEl.height = 600;
+// canvasEl.width = 600;
+
+// canvasContext.fillStyle = "white";
+// canvasContext.fillRect(100, 200, 50, 50);
+// canvasContext.fillStyle = "rgba(214, 200, 167, 0.5)";
+// canvasContext.fillRect(50, 50, 50, 50);
+// canvasContext.fillRect(100, 100, 50, 60);
+// canvasContext.fillRect(150, 150, 50, 50);
+// canvasContext.fillRect(200, 200, 50, 50);
+// canvasContext.fillRect(250, 250, 50, 50);
+// canvasContext.fillRect(300, 300, 50, 50);
+// canvasContext.fillRect(350, 350, 50, 50);
+// canvasContext.fillRect(400, 400, 50, 50);
+// canvasContext.fillRect(450, 450, 50, 50);
+// canvasContext.fillRect(500, 500, 50, 50);
+// canvasContext.fillRect(550, 550, 50, 50);
+
+// everything you have is all you need
+// it's a good day to have a good day
+
+/*
+my code below
+*/
+
 const canvasEl = document.querySelector("canvas");
 const canvasContext = canvasEl.getContext("2d");
 
 canvasEl.height = 600;
-canvasEl.width = 600;
-
-canvasContext.fillStyle = "white";
+canvasEl.width = 800;
+canvasContext.fillStyle = "aqua";
 canvasContext.fillRect(100, 200, 50, 50);
-canvasContext.fillStyle = "rgba(214, 200, 167, 0.5)";
-canvasContext.fillRect(50, 50, 50, 50);
-canvasContext.fillRect(100, 100, 50, 60);
-canvasContext.fillRect(150, 150, 50, 50);
-canvasContext.fillRect(200, 200, 50, 50);
-canvasContext.fillRect(250, 250, 50, 50);
-canvasContext.fillRect(300, 300, 50, 50);
-canvasContext.fillRect(350, 350, 50, 50);
-canvasContext.fillRect(400, 400, 50, 50);
-canvasContext.fillRect(450, 450, 50, 50);
-canvasContext.fillRect(500, 500, 50, 50);
-canvasContext.fillRect(550, 550, 50, 50);
+canvasContext.fillRect(100, 300, 50, 50);
+canvasContext.fillStyle = "rgba(214, 0, 0, 0.5)";
+canvasContext.fillRect(100, 250, 100, 50);
+canvasContext.fillRect(200, 200, 100, 50);
+canvasContext.fillRect(300, 150, 100, 50);
+canvasContext.fillRect(400, 100, 50, 50);
+canvasContext.fillRect(500, 50, 100, 200);
+canvasContext.fillRect(600, 0, 100, 50);
+canvasContext.fillRect(700, 50, 100, 50);
